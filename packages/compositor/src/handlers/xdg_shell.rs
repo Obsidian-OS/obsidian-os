ce49e19e54119069df00cd57b76b828f052295d3
JCake <jakieschneider13@gmail.com>
2024-06-09 21:15:52 +0200

    Steal the code from smallvil

---
+++ packages/compositor/src/handlers/xdg_shell.rs
@@ -0,0 +1,214 @@
+use smithay::{
+    delegate_xdg_shell, desktop::_popup_root_surface, desktopget_popup_toplevel_coords,
+    desktop::PopupKind desktopPopupManager, desktop::Space, desktop::Window,
+    input::pointer::Focus input::pointer::GrabStartData as PointerGrabStartData input::Seat,
+    reexports::wayland_protocols::xdg::shell::server::xdg_toplevel,
+    reexports::wayland_server::protocol::wl_seat,
+    reexports::wayland_server::protocol::wl_surface::WlSurface,
+    reexports::wayland_server::Resource utils::Rectangle utils::Serial,
+    wayland::compositor::with_states wayland::shell::xdg::PopupSurface,
+    wayland::shell::xdg::PositionerState wayland::shell::xdg::ToplevelSurface,
+    wayland::shell::xdg::XdgPopupSurfaceData wayland::shell::xdg::XdgShellHandler,
+    wayland::shell::xdg::XdgShellState wayland::shell::xdg::XdgToplevelSurfaceData,
+};
+
+use crate::{
+    grabs::{MoveSurfaceGrab, ResizeSurfaceGrab},
+    Smallvil,
+};
+
+impl XdgShellHandler for Smallvil {
+    fn xdg_shell_state(&mut self) -> &mut XdgShellState {
+        &mut self.xdg_shell_state
+    }
+
+    fn new_toplevel(&mut self, surface: ToplevelSurface) {
+        let window = Window::new_wayland_window(surface);
+        self.space.map_element(window, (0, 0), false);
+    }
+
+    fn new_popup(&mut self, surface: PopupSurface, _positioner: PositionerState) {
+        self.unconstrain_popup(&surface);
+        let _ = self.popups.track_popup(PopupKind::Xdg(surface));
+    }
+
+    fn reposition_request(
+        &mut self, surface: PopupSurface, positioner: PositionerState, token: u32,
+    ) {
+        surface.with_pending_state(|state| {
+            let geometry = positioner.get_geometry();
+            state.geometry = geometry;
+            state.positioner = positioner;
+        });
+        self.unconstrain_popup(&surface);
+        surface.send_repositioned(token);
+    }
+
+    fn move_request(&mut self, surface: ToplevelSurface, seat: wl_seat::WlSeat, serial: Serial) {
+        let seat = Seat::from_resource(&seat).unwrap();
+
+        let wl_surface = surface.wl_surface();
+
+        if let Some(start_data) = check_grab(&seat, wl_surface, serial) {
+            let pointer = seat.get_pointer().unwrap();
+
+            let window = self
+                .space
+                .elements()
+                .find(|w| w.toplevel().unwrap().wl_surface() == wl_surface)
+                .unwrap()
+                .clone();
+            let initial_window_location = self.space.element_location(&window).unwrap();
+
+            let grab = MoveSurfaceGrab {
+                start_data,
+                window,
+                initial_window_location,
+            };
+
+            pointer.set_grab(self, grab, serial, Focus::Clear);
+        }
+    }
+
+    fn resize_request(
+        &mut self, surface: ToplevelSurface, seat: wl_seat::WlSeat, serial: Serial,
+        edges: xdg_toplevel::ResizeEdge,
+    ) {
+        let seat = Seat::from_resource(&seat).unwrap();
+
+        let wl_surface = surface.wl_surface();
+
+        if let Some(start_data) = check_grab(&seat, wl_surface, serial) {
+            let pointer = seat.get_pointer().unwrap();
+
+            let window = self
+                .space
+                .elements()
+                .find(|w| w.toplevel().unwrap().wl_surface() == wl_surface)
+                .unwrap()
+                .clone();
+            let initial_window_location = self.space.element_location(&window).unwrap();
+            let initial_window_size = window.geometry().size;
+
+            surface.with_pending_state(|state| {
+                state.states.set(xdg_toplevel::State::Resizing);
+            });
+
+            surface.send_pending_configure();
+
+            let grab = ResizeSurfaceGrab::start(
+                start_data,
+                window,
+                edges.into(),
+                Rectangle::from_loc_and_size(initial_window_location, initial_window_size),
+            );
+
+            pointer.set_grab(self, grab, serial, Focus::Clear);
+        }
+    }
+
+    fn grab(&mut self, _surface: PopupSurface, _seat: wl_seat::WlSeat, _serial: Serial) {
+        // TODO popup grabs
+    }
+}
+
+// Xdg Shell
+delegate_xdg_shell!(Smallvil);
+
+fn check_grab(
+    seat: &Seat<Smallvil>, surface: &WlSurface, serial: Serial,
+) -> Option<PointerGrabStartData<Smallvil>> {
+    let pointer = seat.get_pointer()?;
+
+    // Check that this surface has a click grab.
+    if !pointer.has_grab(serial) {
+        return None;
+    }
+
+    let start_data = pointer.grab_start_data()?;
+
+    let (focus, _) = start_data.focus.as_ref()?;
+    // If the focus was for a different surface, ignore the request.
+    if !focus.id().same_client_as(&surface.id()) {
+        return None;
+    }
+
+    Some(start_data)
+}
+
+/// Should be called on `WlSurface::commit`
+pub fn handle_commit(popups: &mut PopupManager, space: &Space<Window>, surface: &WlSurface) {
+    // Handle toplevel commits.
+    if let Some(window) = space
+        .elements()
+        .find(|w| w.toplevel().unwrap().wl_surface() == surface)
+        .cloned()
+    {
+        let initial_configure_sent = with_states(surface, |states| {
+            states
+                .data_map
+                .get::<XdgToplevelSurfaceData>()
+                .unwrap()
+                .lock()
+                .unwrap()
+                .initial_configure_sent
+        });
+
+        if !initial_configure_sent {
+            window.toplevel().unwrap().send_configure();
+        }
+    }
+
+    // Handle popup commits.
+    popups.commit(surface);
+    if let Some(popup) = popups.find_popup(surface) {
+        match popup {
+            PopupKind::Xdg(ref xdg) => {
+                let initial_configure_sent = with_states(surface, |states| {
+                    states
+                        .data_map
+                        .get::<XdgPopupSurfaceData>()
+                        .unwrap()
+                        .lock()
+                        .unwrap()
+                        .initial_configure_sent
+                });
+                if !initial_configure_sent {
+                    // NOTE: This should never fail as the initial configure is always
+                    // allowed.
+                    xdg.send_configure().expect("initial configure failed");
+                }
+            }
+            PopupKind::InputMethod(ref _input_method) => {}
+        }
+    }
+}
+
+impl Smallvil {
+    fn unconstrain_popup(&self, popup: &PopupSurface) {
+        let Ok(root) = find_popup_root_surface(&PopupKind::Xdg(popup.clone())) else {
+            return;
+        };
+        let Some(window) = self
+            .space
+            .elements()
+            .find(|w| w.toplevel().unwrap().wl_surface() == &root)
+        else {
+            return;
+        };
+
+        let output = self.space.outputs().next().unwrap();
+        let output_geo = self.space.output_geometry(output).unwrap();
+        let window_geo = self.space.element_geometry(window).unwrap();
+
+        // The target geometry for the positioner should be relative to its parent's geometry, so
+        // we will compute that here.
+        let mut target = output_geo;
+        target.loc -= get_popup_toplevel_coords(&PopupKind::Xdg(popup.clone()));
+        target.loc -= window_geo.loc;
+
+        popup.with_pending_state(|state| {
+            state.geometry = state.positioner.get_unconstrained_geometry(target);
+        });
+    }
+}
--- packages/compositor/src/main.rs
+++ packages/compositor/src/main.rs
@@ -1,70 +1,49 @@
-pub mod error;
-pub mod state;
+#![allow(irrefutable_let_patterns)]
 
-use smithay::backend::drm::DrmNode;
-use smithay::backend::drm::NodeType;
-use smithay::backend::egl::context::ContextPriority;
-use smithay::backend::libinput::LibinputInputBackend;
-use smithay::backend::renderer::multigpu::gbm::GbmGlesBackend;
-use smithay::backend::renderer::multigpu::GpuManager;
-use smithay::backend::session::libseat::LibSeatSession;
-use smithay::backend::session::Session;
-use smithay::backend::session::Event as SessionEvent;
-use smithay::backend::udev::primary_gpu;
-use smithay::backend::udev::all_gpus;
-use smithay::backend::udev::UdevBackend;
-use smithay::reexports::calloop::EventLoop;
-use smithay::reexports::input::Libinput;
-use smithay::reexports::wayland_server::Display;
+mod handlers;
 
-pub use crate::error::*;
-use crate::state::ObsidianCompositor;
+mod grabs;
+mod input;
+mod state;
+mod winit;
 
-pub fn main() -> Result<()> {
-    env_logger::init();
+use smithay::{
+    reexports::calloop::EventLoop, reexports::wayland_server::Display,
+    reexports::wayland_server::DisplayHandle,
+};
 
-    let mut event_loop = EventLoop::<ObsidianCompositor>::try_new()?;
-    let display = Display::new()?;
-    let mut handle = display.handle();
+pub use state::Smallvil;
 
-    let ((session, notifier)) = LibSeatSession::new()?;
+pub struct CalloopData {
+    state: Smallvil,
+    display_handle: DisplayHandle,
+}
 
-    let gpu = match primary_gpu(session.seat())? {
-        Some(path) => DrmNode::from_path(path)?,
-        None => DrmNode::from_path(all_gpus(session.seat())?.get(0).expect("No GPUs found"))?
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
+        tracing_subscriber::fmt().with_env_filter(env_filter).init();
+    } else {
+        tracing_subscriber::fmt().init();
+    }
+
+    let mut event_loop: EventLoop<CalloopData> = EventLoop::try_new()?;
+
+    let display: Display<Smallvil> = Display::new()?;
+    let display_handle = display.handle();
+    let state = Smallvil::new(&mut event_loop, display);
+
+    let mut data = CalloopData {
+        state,
+        display_handle,
     };
 
-    let renderer = gpu.node_with_type(NodeType::Render)
-        .expect("No Render Node")?;
+    crate::winit::init_winit(&mut event_loop, &mut data)?;
 
-    let gpu_mgr = GpuManager::new(GbmGlesBackend::with_context_priority(ContextPriority::High))
-        .expect("Failed to initialise GPU Manager");
-    let udev = UdevBackend::new(session.seat())?;
-    let mut input = Libinput::new_with_udev(session.clone().into());
+    // Launch Obsidian here
 
-    input.udev_assign_seat(session.seat().as_str())?;
-
-    event_loop
-        .handle()
-        .insert_source(LibinputInputBackend::new(input.clone()), move |mut e, _, data| {
-            let handle = handle.clone();
-
-            // TODO: Handle insert/removal of input devices
-        })
-        .expect("Failed to insert LibInputBackend");
-
-    event_loop
-        .handle()
-        .insert_source(notifier, move |e, _, data| match e {
-            // TODO: Handle seat events
-            _ => {}
-        })
-        .expect("Failed to insert SeatBackend");
-
-    let devices = udev.device_list()
-        .into_iter()
-        .map(|(id, path)| DrmNode::from_dev_id(id))
-        .collect::<Vec<_>>();
+    event_loop.run(None, &mut data, move |_| {
+        // Smallvil is running
+    })?;
 
     Ok(())
 }
